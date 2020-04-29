77da5eb660ffb72a0be0139a16ba82937804b59e
Harsh Pal <harp3401@outlook.com>
2020-04-29 15:35:08 +0530

    Update Day2_Operators.py

--- Hackerrank/30-Days-of-code/Day2_Operators.py
+++ Hackerrank/30-Days-of-code/Day2_Operators.py
@@ -8,6 +8,9 @@ import sys
 
 # Complete the solve function below.
 def solve(meal_cost, tip_percent, tax_percent):
+    tax1 = meal_cost*(float(tip_percent)/100)
+    tax2 = meal_cost*(float(tax_percent)/100)
+    return meal_cost+tax1+tax2
 
 if __name__ == '__main__':
     meal_cost = float(input())
@@ -16,4 +19,4 @@ if __name__ == '__main__':
 
     tax_percent = int(input())
 
-    solve(meal_cost, tip_percent, tax_percent)
+    print(round(solve(meal_cost, tip_percent, tax_percent)))
